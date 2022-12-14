dc06ad97593ec4124a8aaef6b921164aa0bf0e78
rafaeelnunesf <rafaelnfsq@gmail.com>
2022-12-14 15:00:28 -0300

    feat: create user entity

---
+++ src/api/user/user.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'varchar', length: 120 })
+  public name: string;
+
+  @Column({ type: 'varchar', length: 120 })
+  public email: string;
+}
