df26eb2350ca20abf04eb4245964d64eead8f9ef
rafaeelnunesf <rafaelnfsq@gmail.com>
2022-12-19 14:17:34 -0300

    feat: create jwt strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,23 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UserService } from 'src/api/user/user.service';
+import { UserFromJwt } from '../models/UserFromJwt';
+import { UserPayload } from '../models/UserPayload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly usersService: UserService,
-  ) {
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'bananinha',
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
-  async validate(payload: any) {
-    return {
+  async validate(payload: UserPayload): Promise<UserFromJwt> {
+    return await {
       id: payload.sub,
       email: payload.email,
       name: payload.name,
