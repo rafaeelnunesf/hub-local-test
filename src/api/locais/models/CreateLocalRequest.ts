172f8f596c0e41c94d3bc1ce27e21febca1a4399
Rafael Nunes <rafaelnfsq@gmail.com>
2022-12-21 09:17:59 -0300

    fix: improve interface name

--- src/api/locais/models/CreateLocalRequest.ts
+++ src/api/locais/models/CreateLocalRequest.ts
@@ -2,7 +2,7 @@ import { Request } from 'express';
 import { Empresa } from 'src/api/empresas/empresa.entity';
 import { CreateLocalDto } from '../dto/create-local.dto';
 
-export interface CreateLocalRequest extends Request {
+export interface LocalRequest extends Request {
   empresa: Empresa;
   body: CreateLocalDto;
 }
